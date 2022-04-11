5ac9504897a338c3a485613cf13a81a11865287a
Mohammednur <mamez7878@gmail.com>
2022-04-11 13:17:39 +0000

    commit Positive Or Negative

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlin.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * A C code file differentiates between -ve and +ve
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n)
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative", n)
+	}
+	else
+	{
+		printf("%d is zero", n)
+	}
+
+	return (0);
+}

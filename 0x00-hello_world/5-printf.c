9ec5ded42d8e52632db322565cc3137453f06199
Mohammednur <mamez7878@gmail.com>
2022-04-11 09:17:44 +0000

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C code for printing message using printf()
+ * A C code that prints message using printf
  *
- * Return: Always 0 (Sucsess)
+ * Return 0 (Success)
  */
 
-int main(void)
-{
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

08542a9980315c5fc7aa929defa048a6c3f145da
Mohammednur <mamez7878@gmail.com>
2022-04-12 13:34:25 +0000

    commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,13 @@
  *
  * Return: Always 0 (success)
  */
+
 void print_alphabet(void)
 {
 	int ch;
-	for(ch = 'a'; ch < 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
+
 	_putchar('\n');
 }

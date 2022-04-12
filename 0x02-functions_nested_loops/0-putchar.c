b69a112f27426763a354b57446781a957cd137e1
Mohammednur <mamez7878@gmail.com>
2022-04-12 11:52:32 +0000

    commit 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printing '_putchar' with out using printf or puts
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	char ch = "_putchar";
+
+	putchar(ch);
+
+	return (0);
+}

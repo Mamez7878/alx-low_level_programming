cf70eee409bcefd8377f64618e583986210bf67b
Mohammednur <mamez7878@gmail.com>
2022-04-12 14:18:35 +0000

    Identify the Lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function identifies lowercase letters
+ *
+ * Return - Always 0 (succus)
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (0);
+
+	return (0);
+}

191346231793e2673c5125eb85e43d4fc49a43bf
Mohammednur <mamez7878@gmail.com>
2022-04-12 13:24:32 +0000

    commit 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * printing alphabets using _putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+	while(ch = 'a'; ch < 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}

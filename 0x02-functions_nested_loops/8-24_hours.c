203aedda2dd18648004bc888d5b92b4d8fc000c5
Mohammednur <mamez7878@gmail.com>
2022-04-13 12:33:36 +0000

    24 Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,13 @@ void jack_bauer(void)
 
 	for (hr = 0; hr < 24; hr++)
 	{
-		for (mi = 0; mi <= 59; mi++)
+		for (mi = 0; mi < 60; mi++)
 		{
-			_putchar((hr / 10) + 0);
-			_putchar((hr % 10) + 0);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar((mi / 10) + 0);
-			_putchar((mi % 10) + 0);
+			_putchar((mi / 10) + 48);
+			_putchar((mi % 10) + 48);
 			_putchar('\n');
 		}
 	}

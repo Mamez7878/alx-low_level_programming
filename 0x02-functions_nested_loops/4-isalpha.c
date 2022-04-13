a6d75702e5b4a9143924ba2e414d169c85e2272a
Mohammednur <mamez7878@gmail.com>
2022-04-13 07:44:27 +0000

    Differ Upper and lowercae from other

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that differs lowercase and uppercase
+ *
+ * @c: checks input of a function
+ *
+ * Return: returns 1 if upper or lower case
+ * and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 91 || c >= 97 && c <= 123)
+		return (1);
+
+	return (0);
+}

7fd2fbeee0630be2fcc3f375c34e111881b70a7b
Mohammednur <mamez7878@gmail.com>
2022-04-13 11:04:31 +0000

    Tell the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,7 @@
  *
  * @n: a given number
  *
- * Return: return 1 if positive, 0 if zero and
- * -1 if negative
+ * Return: return 1 if positive, 0 if zero and 1 if negative
  */
 
 int print_sign(int n)

696a3d74e77f92de5be87eef260ad656b871775a
Mohammednur <mamez7878@gmail.com>
2022-04-13 12:31:26 +0000

    24 Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints like an hour and minte
+ */
+
+void jack_bauer(void)
+{
+	int hr, mi;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (mi = 0; mi <= 59; mi++)
+		{
+			_putchar((hr / 10) + 0);
+			_putchar((hr % 10) + 0);
+			_putchar(':');
+			_putchar((mi / 10) + 0);
+			_putchar((mi % 10) + 0);
+			_putchar('\n');
+		}
+	}
+}

394ea121be3808b94bee9cb0317d8b7b7af8455d
Mohammednur <mamez7878@gmail.com>
2022-04-14 07:53:36 +0000

    commit Main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,16 +11,13 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming: ");
-
 	/**
 	* i = 0;
 	*
-	* while (i < 10)
-	* {
+	* while (i < 10) {
 	*	putchar(i);
 	* }
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

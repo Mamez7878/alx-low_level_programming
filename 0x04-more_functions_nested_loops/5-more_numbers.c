ef901ad9cddbaef6c67ef2c6a338e2bb799e3e47
Mohammednur <mamez7878@gmail.com>
2022-04-15 08:35:06 +0000

    Print 0 - 9 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,14 @@ void more_numbers(void)
 	{
 		int j;
 
-		for (j = 0; j <= 14; j ++)
-			_putchar(j + 48);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((x % 10) + 48);
+		}
 		_putchar('\n');
 	}
 }

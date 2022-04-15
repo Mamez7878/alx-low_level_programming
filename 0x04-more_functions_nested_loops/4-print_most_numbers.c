a4f677c80532333e76bd3ae6af401f65500643c5
Mohammednur <mamez7878@gmail.com>
2022-04-15 08:19:47 +0000

    Print numbers form 0 - 9 without 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - printing most numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i != 2 && 4;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + 48);
+	_putchar('\n');
+}

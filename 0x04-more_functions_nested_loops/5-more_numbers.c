ff7105f0ef33819cba1776c69afb9043c785a1da
Mohammednur <mamez7878@gmail.com>
2022-04-15 08:29:36 +0000

    Print 0 - 9 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 - 9 x10
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j ++)
+			_putchar(j + 48);
+		_putchar('\n');
+	}
+}

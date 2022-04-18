b84db16c8b93b397722b4a3e4dff864ef75b7c48
Mohammednur <mamez7878@gmail.com>
2022-04-18 11:00:48 +0000

    Reversing The String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,22 @@
 
 void rev_string(char *s)
 {
-	int n;
+	int n, i, j;
+	char c1, c2;
 
 	for (n = 0; s[n] != '\0'; n++)
 		;
 
-	for (n--; n >= 0; n--)
-		s[n] = s;
+	j = n - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		c1 = s[i];
+		c2 = s[j];
+		s[i] = c2;
+		s[j] = c1;
+		j--;
+		i++;
+	}
 }

e9d8675995ae9fa49f6f2a185f46891558ba1f07
Mohammednur <mamez7878@gmail.com>
2022-04-18 11:21:09 +0000

    Prints every 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character 
+ * puts2 - prints every other character
  * starting from the first character
  *
- * @str - a string
+ * @str: a string
  */
 
 void puts2(char *str)
 {
-	int n;
+	int i = 0;
 
-	for (n = 0; str[n] <= *str; n++)
-		str[n] = n + 2;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }

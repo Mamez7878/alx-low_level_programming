b0dda7dde9a13a0042bfe5f3e261951c912642d4
Mohammednur <mamez7878@gmail.com>
2022-04-19 08:11:32 +0000

    Convert string to an intiger

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to intiger
+ *
+ * @s: a string
+ *
+ * Return - 0 if there is no any numbers
+ */
+
+int _atoi(char *s)
+{
+	atoi(s);
+}

45745ab51f810b350bb9148a738beb11d8d87145
Mohammednur <mamez7878@gmail.com>
2022-04-19 07:40:25 +0000

    Printing n items of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,12 +4,24 @@
  * print_array - prints a array of n items
  *
  * @n: the number of items
+ * @a: an array
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
-		printf("%d, ", a[i]);
+	for (i = 0; i <= n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", a[i]);
+		}
+		else if (i != (n - 1))
+		{
+			prinrf("%d", a[i]);
+			printf(", ");
+		}
+	}
+	printf("\n");
 }

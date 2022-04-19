4f022ab232c4037602b3a16caef535cdcd59220e
Mohammednur <mamez7878@gmail.com>
2022-04-19 07:52:42 +0000

    Copy the string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies string 1 to the other
+ *
+ * @dest: the first string
+ * @src: the second string
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}

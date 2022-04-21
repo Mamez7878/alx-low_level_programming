8498b7a18dc3b1a56a58d47b6bba4e8cada1385d
Mohammednur <mamez7878@gmail.com>
2022-04-21 08:14:09 +0000

    Printing in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 90; i--)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}

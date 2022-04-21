e6ec5c330d7799694b3cfb1fe6cdfb97e0d2df90
Mohammednur <mamez7878@gmail.com>
2022-04-21 07:23:37 +0000

    Commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 'a';
+	int j = 'A';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	return 0;
+}

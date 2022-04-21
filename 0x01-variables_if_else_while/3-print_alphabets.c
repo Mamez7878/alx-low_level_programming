878dfb505b64f0763878e0894a2e644ea21289ee
Mohammednur <mamez7878@gmail.com>
2022-04-21 07:41:21 +0000

    Commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 97; i <= 122; i++)
 		putchar(i + 48);
-	for (i = 65; i <= 90; i++)
-		putchar(i + 48);
+	for (j = 65; j <= 90; j++)
+		putchar(j + 48);
 	return (0);
 }

ff5ed6aa7a924e662b8ad1e55c2646c68c9889de
Mohammednur <mamez7878@gmail.com>
2022-04-21 09:36:39 +0000

    Prints a Number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,5 +8,25 @@
 
 void print_number(int n)
 {
-	putchar(n + 48);
+	int i = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while ((n / i) >= 10)
+			i *= 10;
+
+		while (i > 0)
+		{
+			_putchar((n / i) + '0');
+			n %= i;
+			i /= 10;
+		}
+	}
 }

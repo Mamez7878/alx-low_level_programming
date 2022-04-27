8522e62c21b5b2ec80f7ed91e47d7d7a5ebc9a57
Mohammednur <mamez7878@gmail.com>
2022-04-27 12:05:18 +0000

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	if (s[i] > 0)
-		return;
-
-	i++;
-
-	_puts_recursion(_putchar(s[i]));
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }

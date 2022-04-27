314bdd978afe1f7b6bcc153dd5afce55668aed09
Mohammednur <mamez7878@gmail.com>
2022-04-27 13:44:05 +0000

    Calculate x^y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,17 +12,15 @@
 
 int _pow_recursion(int x, int y)
 {
-	int n;
-
 	if (y < 0)
 		return (-1);
 
 	else if (x == 0)
 		return (1);
-	
+
 	else if (y % 2 == 0)
 	{
-		n = _pow_recursion(x, y / 2);
+		int n = _pow_recursion(x, y / 2);
 		return (n * n);
 	}
 	else

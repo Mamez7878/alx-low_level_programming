b166ddf28c7c5b8b4e37348e67c4eb368564c717
Mohammednur <mamez7878@gmail.com>
2022-04-29 13:52:06 +0000

    slight changes in *

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -10,12 +10,12 @@
  * Return: dest
  */
 
-*_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		src[i] = dest[i];
 
-	return (*dest);
+	return (dest);
 }

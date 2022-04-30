eba10c4c04f03e1b529a3943aac2f20af79eafb6
mamez7878 <mamez7878@gmail.com>
2022-04-30 11:44:14 +0300

    Multiply The Arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,20 +9,19 @@
  * Return: 0 or 1
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int k;
+	int m;
 
-	if (argc != 3 )
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		k = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", k);
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
 	}
-
 	return (0);
 }

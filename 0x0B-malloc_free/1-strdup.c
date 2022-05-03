dde83f7b1c882da367294f8681b5fffb55576c34
Mohammednur <mamez7878@gmail.com>
2022-05-03 08:26:11 +0000

    Duplicates a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,16 +13,19 @@ char *_strdup(char *str)
 	int i, l;
 	char *s;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (l = 0; str[l] != '\0'; l++)
 		;
 
 	s = malloc((l + 1) * sizeof(char));
 
+	if (s == NULL)
+		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 		s[i] = str[i];
-
-	if (s == NULL || str == NULL)
-		return (NULL);
 
 	return (s);
 }

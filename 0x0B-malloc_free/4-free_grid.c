1869dd8f3a8ebc4cfcd518dd25af4c58487063a7
Mohammednur <mamez7878@gmail.com>
2022-05-04 08:14:58 +0000

    Frees a grid in memory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * free_gird - frees a 2 dimentional grid created by malloc
+ * free_grid - frees a 2 dimentional grid created by malloc
  *
  * @grid: the 2d array
  * @height:the height of the grid
  */
 
-void free_gird(int **gird, int height)
+void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{

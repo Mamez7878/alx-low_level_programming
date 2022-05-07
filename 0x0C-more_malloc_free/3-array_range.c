53885098fbfc34faac7e9faca5e7cf185d09c7f3
mamez7878 <mamez7878@gmail.com>
2022-05-07 19:38:37 +0300

    A function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,58 @@
-#include "main/h"
+#include "main.h"
+
 
 /**
- * array_range - creates an array of integers
+ * _memcpy - copies memory
+ * @dest: destination
+ * @src: source
+ * @n: size of memory to copy
  *
- * @min: min number of elements
- * @max: max number of elements
- *
- * Return: array pointer address
- *         NULL if it fails
-*/
-
-int *array_range(int min, int max)
+ * Return: Returns memory copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i, *array;
+	unsigned int i;
 
-	if (min > max)
-		return (NULL);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
 
-	array = malloc((max - min + 1) * sizeof(int));
-	if (array == NULL)
-		return (NULL);
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to modify
+ * @old_size: current size of memory
+ * @new_size: size memory will now have
+ *
+ * Return: Pointer to reallocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ptr2;
 
-	for (i = 0; min <= max; i++)
+	if (old_size == new_size)
+		return (ptr);
+
+	if (ptr == NULL)
 	{
-		array[i] = min;
-		min++;
+		ptr2 = malloc(new_size);
+		if (ptr2 == 0)
+			return (0);
+		free(ptr);
+		return (ptr2);
 	}
-	return (array);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	ptr2 = malloc(new_size);
+	if (ptr2 == 0)
+		return (0);
+
+	_memcpy(ptr2, ptr, old_size);
+	free(ptr);
+	return (ptr2);
 }
